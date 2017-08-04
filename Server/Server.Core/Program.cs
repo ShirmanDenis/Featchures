8ef153124885b1f5ef680a479821b2a760c2a188
Denis <denshi747@gmail.com>
2017-08-04 20:54:05 +0500

    base packages

---
+++ Server/Server.Core/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Server.Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var connection = new SQLiteConnection())
+            {
+                
+            }
+        }
+    }
+}
