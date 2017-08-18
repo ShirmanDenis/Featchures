ec62e0a39421a3c06d63f18d5dbcf2a10260d066
Denis <denshi747@gmail.com>
2017-08-18 23:11:24 +0500

    Server now handle context

--- Server/Server.Core/Program.cs
+++ Server/Server.Core/Program.cs
@@ -1,10 +1,37 @@
-﻿namespace Server.Core
+﻿using System;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Server.Core
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var server = new Server(true);
+            using (var server = new Server())
+            {
+                server.Start("http://+:8080/");
+
+                Console.Write("Send to server: ");
+                var data = Console.ReadLine();
+
+                const string baseAdress = "http://localhost:8080/";
+                var uri = new Uri(baseAdress);
+                var webClient = new WebClient { BaseAddress = baseAdress };
+
+                webClient.UploadString(uri, data);
+
+                Console.Write("Sending..");
+            }
+        }
+
+        private static async void SendData()
+        {
+            await Task.Run(() =>
+            {
+
+            });
         }
     }
 }
--- Server/Server.Core/Server/Server.cs
+++ Server/Server.Core/Server/Server.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using NLog;
+using NLog.Config;
 using SQLite;
 
 namespace Server.Core
@@ -21,6 +24,7 @@ namespace Server.Core
         private Thread _listenerThread;
         private readonly Dictionary<string, int> _threadsReg = new Dictionary<string, int>();
         private int currentId = 0;
+        private Logger _logger = LogManager.GetCurrentClassLogger();
 
         public void Start(string prefix)
         {
@@ -39,6 +43,7 @@ namespace Server.Core
                     IsBackground = true,
                     Priority = ThreadPriority.Highest
                 };
+                _listenerThread.Start();
 
                 IsRunning = true;
             }
@@ -58,30 +63,56 @@ namespace Server.Core
                 _listenerThread.Join();
 
                 IsRunning = false;
+
+                _logger.Info("Server stopped");
             }
         }
 
         public void Listen()
         {
+            _logger.Info("Server start listen");
             while (true)
             {
                 try
                 {
                     if (_httpListener.IsListening)
                     {
-                        // What should I do??
+                       var context = _httpListener.GetContext();
+                       Task.Factory.StartNew(() => HandleInputContextAsync(context));
                     }
                     else
                     {
                         Thread.Sleep(0);
                     }
                 }
-                catch (Exception e)
+                catch (Exception error)
                 {
-                    Console.WriteLine(e);
-                    throw;
+                    _logger.Error("Error: " + error.Message);
+                    _logger.Error("Object: " + error.Source);
+                    _logger.Error("Мethod: " + error.TargetSite);
+                    _logger.Error("StackTrace: " + error.StackTrace);
+                    _logger.Error("=================================================");
                 }
             }
+        }
+
+        private async void HandleInputContextAsync(HttpListenerContext context)
+        {
+            _logger.Info("In HandleInputContext()");
+
+            var request = context.Request;
+            string result;
+            using (var reader = new StreamReader(request.InputStream))
+            {
+               result = await reader.ReadToEndAsync();
+            }
+
+            using (var writer = new StreamWriter(context.Response.OutputStream))
+            {
+                var wtf = "wtf";
+                await writer.WriteLineAsync($"Result {(string.IsNullOrEmpty(result) ? result : wtf)}");
+            }
+            context.Response.Close();
         }
 
         public void Dispose()
