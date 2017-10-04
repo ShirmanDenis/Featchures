bfcea35feba78482d69bec51728d915957b74c58
Denis <denshi747@gmail.com>
2017-10-04 11:32:02 +0500

    sin(X) c++ GL

--- OpenGL/C#/GLBuilding/Test/MainWindow.Designer.cs
+++ OpenGL/C#/GLBuilding/Test/MainWindow.Designer.cs
@@ -28,59 +28,43 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.AnT = new Tao.Platform.Windows.SimpleOpenGlControl();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.timer = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // AnT
             // 
             this.AnT.AccumBits = ((byte)(0));
-            this.AnT.AutoCheckErrors = false;
+            this.AnT.AutoCheckErrors = true;
             this.AnT.AutoFinish = false;
             this.AnT.AutoMakeCurrent = true;
             this.AnT.AutoSwapBuffers = true;
             this.AnT.BackColor = System.Drawing.Color.Black;
             this.AnT.ColorBits = ((byte)(32));
             this.AnT.DepthBits = ((byte)(16));
-            this.AnT.Location = new System.Drawing.Point(12, 12);
+            this.AnT.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.AnT.Location = new System.Drawing.Point(0, 0);
             this.AnT.Name = "AnT";
-            this.AnT.Size = new System.Drawing.Size(840, 587);
+            this.AnT.Size = new System.Drawing.Size(622, 534);
             this.AnT.StencilBits = ((byte)(0));
             this.AnT.TabIndex = 0;
-            this.AnT.Load += new System.EventHandler(this.simpleOpenGlControl1_Load);
+            this.AnT.ClientSizeChanged += new System.EventHandler(this.AnT_ClientSizeChanged);
+            this.AnT.MouseMove += new System.Windows.Forms.MouseEventHandler(this.AnT_MouseMove);
             // 
-            // button1
+            // timer
             // 
-            this.button1.Location = new System.Drawing.Point(858, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Sphere";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(858, 41);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
             // 
             // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(945, 600);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(622, 534);
             this.Controls.Add(this.AnT);
             this.Name = "MainWindow";
             this.Text = "GLBuild3D";
-            this.Shown += new System.EventHandler(this.MainWindow_Shown);
+            this.Load += new System.EventHandler(this.MainWindow_Load);
             this.ResumeLayout(false);
 
         }
@@ -88,8 +72,7 @@
         #endregion
 
         private Tao.Platform.Windows.SimpleOpenGlControl AnT;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer;
     }
 }
 
--- OpenGL/C#/GLBuilding/Test/MainWindow.cs
+++ OpenGL/C#/GLBuilding/Test/MainWindow.cs
@@ -1,13 +1,39 @@
 ﻿using System;
 using System.Windows.Forms;
-using GLBuilding.Core;
-using GLBuilding.Core.BuildingBase;
+using Tao.FreeGlut;
+using Tao.OpenGl;
 
 namespace Test
 {
     public partial class MainWindow : Form
     {
-        private BuildingArea _buildingArea;
+        // размеры окна 
+        double ScreenW, ScreenH;
+
+        // отношения сторон окна визуализации 
+        // для корректного перевода координат мыши в координаты, 
+        // принятые в программе 
+
+        private double devX;
+        private double devY;
+
+        // массив, который будет хранить значения x,y точек графика 
+        private double[,] GrapValuesArray;
+        // количество элементов в массиве 
+        private int elements_count = 0;
+
+        // флаг, означающий, что массив с значениями координат графика пока еще не заполнен 
+        private bool not_calculate = true;
+
+        // номер ячейки массива, из которой будут взяты координаты для красной точки 
+        // для визуализации текущего кадра 
+        private int pointPosition = 0;
+
+        // вспомогательные переменные для построения линий от курсора мыши к координатным осям 
+        double lineX, lineY;
+
+        // текущение координаты курсора мыши 
+        double Mcoord_X = 0, Mcoord_Y = 0;
 
         public MainWindow()
         {
@@ -16,26 +42,274 @@ namespace Test
             AnT.InitializeContexts();
         }
 
-        private void simpleOpenGlControl1_Load(object sender, EventArgs e)
+        private void AnT_MouseMove(object sender, MouseEventArgs e)
         {
-            var ctx = new BuildContext() { ClearColor = Color.Red()};
-            _buildingArea = new BuildingArea(ctx);
+            // сохраняем координаты мыши 
+            Mcoord_X = e.X;
+            Mcoord_Y = e.Y;
+
+            // вычисляем параметры для будущей дорисовки линий от указателя мыши к координатным осям. 
+            lineX = devX * e.X;
+            lineY = (float)(ScreenH - devY * e.Y);
+
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void MainWindow_Load(object sender, EventArgs e)
         {
-            _buildingArea.DrawSphere();
+            // инициализация библиотеки glut 
+            Glut.glutInit();
+            // инициализация режима экрана 
+            Glut.glutInitDisplayMode(Glut.GLUT_RGB | Glut.GLUT_DOUBLE);
+
+            // установка цвета очистки экрана (RGBA) 
+            Gl.glClearColor(255, 255, 255, 1);
+
+            // установка порта вывода 
+            Gl.glViewport(0, 0, AnT.Width, AnT.Height);
+
+            // активация проекционной матрицы 
+            Gl.glMatrixMode(Gl.GL_PROJECTION);
+            // очистка матрицы 
+            Gl.glLoadIdentity();
+
+            // определение параметров настройки проекции в зависимости от размеров сторон элемента AnT. 
+            if ((float)AnT.Width <= (float)AnT.Height)
+            {
+                ScreenW = 30.0;
+                ScreenH = 30.0 * (float)AnT.Height / (float)AnT.Width;
+                Glu.gluOrtho2D(0.0, ScreenW, 0.0, ScreenH);
+            }
+            else
+            {
+                ScreenW = 30.0 * (float)AnT.Width / (float)AnT.Height;
+                ScreenH = 30.0;
+                Glu.gluOrtho2D(0.0, 30.0 * (float)AnT.Width / (float)AnT.Height, 0.0, 30.0);
+            }
+
+            // сохранение коэффициентов, которые нам необходимы для перевода координат указателя в оконной системе в координаты, 
+            // принятые в нашей OpenGL сцене 
+            devX = (float)ScreenW / (float)AnT.Width;
+            devY = (float)ScreenH / (float)AnT.Height;
+
+            // установка объектно-видовой матрицы 
+            Gl.glMatrixMode(Gl.GL_MODELVIEW);
+
+            // старт счетчика, отвечающего за вызов функции визуализации сцены 
+            timer.Start();
+
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            // если мы дошли до последнего элемента массива 
+            if (pointPosition == elements_count - 1)
+                pointPosition = 0; // переходим к начальному элементу 
+
+            // функция визуализации 
+            Draw();
+
+            // переход к следующему элементу массива 
+            pointPosition++;
+
+        }
+
+        // функция визуализации текста 
+        private void PrintText2D(float x, float y, string text)
+        {
+
+            // устанавливаем позицию вывода растровых символов 
+            // в переданных координатах x и y. 
+            Gl.glRasterPos2f(x, y);
+
+            // в цикле foreach перебираем значения из массива text, 
+            // который содержит значение строки для визуализации 
+            foreach (char char_for_draw in text)
+            {
+                // символ C визуализируем с помощью функции glutBitmapCharacter, используя шрифт GLUT_BITMAP_9_BY_15. 
+                Glut.glutBitmapCharacter(Glut.GLUT_BITMAP_9_BY_15, char_for_draw);
+            }
+
+        }
+
+        private void AnT_ClientSizeChanged(object sender, EventArgs e)
+        {
+            // сохранение коэффициентов, которые нам необходимы для перевода координат указателя в оконной системе в координаты, 
+            // принятые в нашей OpenGL сцене 
+            devX = (float)ScreenW / (float)AnT.Width;
+            devY = (float)ScreenH / (float)AnT.Height;
+        }
+
+        private void functionCalculation()
+        {
+
+            // определение локальных переменных X и Y 
+            float x = 0, y = 0;
+
+            // инициализация массива, который будет хранить значение 300 точек, 
+            // из которых будет состоять график 
+
+            GrapValuesArray = new double[300, 2];
+
+            // счетчик элементов массива 
+            elements_count = 0;
+
+            // вычисления всех значений y для x, принадлежащего промежутку от -15 до 15 с шагом в 0.01f 
+            for (x = -15; x < 15; x += 0.1f)
+            {
+                // вычисление y для текущего x 
+                // по формуле y = (float)Math.Sin(x)*3 + 1; 
+                // эта строка задает формулу, описывающую график функции для нашего уравнения y = f(x). 
+                y = (float)Math.Sin(x) * 3 + 1;
+
+                // запись координаты x 
+                GrapValuesArray[elements_count, 0] = x;
+                // запись координаты y 
+                GrapValuesArray[elements_count, 1] = y;
+                // подсчет элементов 
+                elements_count++;
+
+            }
+
+            // изменяем флаг, сигнализировавший о том, что координаты графика не вычислены 
+            not_calculate = false;
+
+        }
+
+        // визуализация графика 
+        private void DrawDiagram()
+        {
+
+            // проверка флага, сигнализирующего о том, что координаты графика вычислены 
+            if (not_calculate)
+            {
+                // если нет, то вызываем функцию вычисления координат графика 
+                functionCalculation();
+            }
+
+            // стартуем отрисовку в режиме визуализации точек 
+            // объединяемых в линии (GL_LINE_STRIP) 
+            Gl.glBegin(Gl.GL_LINE_STRIP);
+
+            // рисуем начальную точку 
+            Gl.glVertex2d(GrapValuesArray[0, 0], GrapValuesArray[0, 1]);
+
+            // проходим по массиву с координатами вычисленных точек 
+            for (int ax = 1; ax < elements_count; ax += 2)
+            {
+                // передаем в OpenGL информацию о вершине, участвующей в построении линий 
+                Gl.glVertex2d(GrapValuesArray[ax, 0], GrapValuesArray[ax, 1]);
+            }
+
+            // завершаем режим рисования 
+            Gl.glEnd();
+
+            // устанавливаем размер точек, равный 5 пикселям 
+            Gl.glPointSize(5);
+            // устанавливаем текущим цветом - красный цвет 
+            Gl.glColor3f(255, 0, 0);
+            // активируем режим вывода точек (GL_POINTS) 
+            Gl.glBegin(Gl.GL_POINTS);
+            // выводим красную точку, используя ту ячейку массива, до которой мы дошли (вычисляется в функции обработчике событий таймера) 
+            Gl.glVertex2d(GrapValuesArray[pointPosition, 0], GrapValuesArray[pointPosition, 1]);
+            // завершаем режим рисования 
+            Gl.glEnd();
+            // устанавливаем размер точек равный единице 
+            Gl.glPointSize(1);
+
+        }
+
+        // функция, управляющая визуализацией сцены 
+        private void Draw()
+        {
+            // очистка буфера цвета и буфера глубины 
+            Gl.glClear(Gl.GL_COLOR_BUFFER_BIT | Gl.GL_DEPTH_BUFFER_BIT);
+
+            // очищение текущей матрицы 
+            Gl.glLoadIdentity();
+
+            // установка черного цвета 
+            Gl.glColor3ub(0, 0, 0);
+
+            // помещаем состояние матрицы в стек матриц 
+            Gl.glPushMatrix();
+
+            // выполняем перемещение в пространстве по осям X и Y 
+            Gl.glTranslated(15, 15, 0);
+
+            // активируем режим рисования (Указанные далее точки будут выводиться как точки GL_POINTS) 
+            Gl.glBegin(Gl.GL_POINTS);
+
+            // с помощью прохода двумя циклами, создаем сетку из точек 
+            for (int ax = -15; ax < 15; ax++)
+            {
+                for (int bx = -15; bx < 15; bx++)
+                {
+                   // вывод точки 
+                    Gl.glVertex2d(ax, bx);
+                }
+            }
+
+            // завершение режима рисования примитивов 
+            Gl.glEnd();
+            // активируем режим рисования, каждые 2 последовательно вызванные команды glVertex 
+            // объединяются в линии 
+            Gl.glBegin(Gl.GL_LINES);
+
+            // далее мы рисуем координатные оси и стрелки на их концах 
+            Gl.glVertex2d(0, -15);
+            Gl.glVertex2d(0, 15);
+
+            Gl.glVertex2d(-15, 0);
+            Gl.glVertex2d(15, 0);
+
+            // вертикальная стрелка 
+            Gl.glVertex2d(0, 15);
+            Gl.glVertex2d(0.1, 14.5);
+            Gl.glVertex2d(0, 15);
+            Gl.glVertex2d(-0.1, 14.5);
+
+            // горизонтальная трелка 
+            Gl.glVertex2d(15, 0);
+            Gl.glVertex2d(14.5, 0.1);
+            Gl.glVertex2d(15, 0);
+            Gl.glVertex2d(14.5, -0.1);
+
+            // завершаем режим рисования 
+            Gl.glEnd();
+
+            // выводим подписи осей "x" и "y" 
+            PrintText2D(15.5f, 0, "x");
+            PrintText2D(0.5f, 14.5f, "y");
+
+            // вызываем функцию рисования графика 
+            DrawDiagram();
+
+            // возвращаем матрицу из стека 
+            Gl.glPopMatrix();
+
+            // выводим текст со значением координат возле курсора 
+            PrintText2D((float)(devX * Mcoord_X + 0.2f),(float) (ScreenH - devY * Mcoord_Y + 0.4), "[ x: " + (devX * Mcoord_X - 15) + " ; y: " + ((float)ScreenH - devY * Mcoord_Y - 15) + "]");
+            
+            // устанавливаем красный цвет 
+            Gl.glColor3f(255, 0, 0);
+
+            // включаем режим рисования линий, для того чтобы нарисовать 
+            // линии от курсора мыши к координатным осям 
+            Gl.glBegin(Gl.GL_LINES);
+
+            Gl.glVertex2d(lineX, 15);
+            Gl.glVertex2d(lineX, lineY);
+            Gl.glVertex2d(15, lineY);
+            Gl.glVertex2d(lineX, lineY);
+
+            Gl.glEnd();
+
+            // дожидаемся завершения визуализации кадра 
+            Gl.glFlush();
+
+            // сигнал для обновление элемента реализующего визуализацию. 
             AnT.Invalidate();
-        }
 
-        private void button2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void MainWindow_Shown(object sender, EventArgs e)
-        {
-            AnT.Invalidate();
         }
     }
 }
